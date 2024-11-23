b43a15e46de3d035c96a89db41aecc9a97dfe9ae
Maurice-Jah <mauricejahwizwid@gmail.com>
2024-11-23 04:48:26 +0100

    Final solution

--- week_3/shopping.py
+++ week_3/shopping.py
@@ -2,7 +2,7 @@
 # if the discount is 20% or higher apply discount else return price
 
 
-def calculate_discount(price,discount_percent):
+def calculate_discount(price, discount_percent):
     if(discount_percent >= 20):
         discount = (discount_percent / 100 ) * price
         total_price = price - discount
@@ -11,5 +11,11 @@ def calculate_discount(price,discount_percent):
         return price
 
 
-final_price = calculate_discount(200,20)
-print(final_price)
+
+# 2. prompt user to enter the original price and discount
+user_input_price = float(input("Please enter the price "))
+user_input_discount = float(input("Please enter the discount "))
+
+final_price = calculate_discount(user_input_price,user_input_discount)
+print(f"The price is {final_price}")
+
